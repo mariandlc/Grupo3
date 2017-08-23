e25dba9c703b2045835869b56416a988decf8295
Robert <robertdibejar@gmail.com>
2017-08-23 17:18:19 -0300

    se cambio la barra de navegacion

--- docs/public/js/main.js
+++ docs/public/js/main.js
@@ -1,8 +1,13 @@
+function checkActual(){
+  var pointer = $('.icon-nav');
+  var id = $(pointer).attr('id');
+  if (id === 'actual')
+    /*$('#actual').css(,);
+    $('#actual').css(,);*/
+  console.log(id);
+}
 $(document).ready(function() {
 
-  $('.section-mobile').click(function() {
-    var id = $(this).attr('id');
-    console.log(id);
-  });
+  checkActual();
 
 });
